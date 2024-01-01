36213110bfcc552f0dc39f078e8cc9b8d5125170
Yuya.Yoshida <145914125+Namazouchan@users.noreply.github.com>
2024-01-01 06:28:37 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    # @user = User.first
+    @user = User.find(params[:id])
+    # => app/views/users/show.html.erb
+  end
+
+  # => GET users/new or /signup
+  def new
+    # app/views/users/new.html.erb
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) 
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user  # => GET users/:id
+      # redirect_to user_path(@user) => GET users/:id
+      # redirect_to user_path(@user.id) GET users/:2
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+    # Strong Parameters
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
+
 end
