a68270d3dfeb7a55a111846e321116f02eb5356a
Yuya.Yoshida <145914125+Namazouchan@users.noreply.github.com>
2024-01-03 13:39:17 +0000

    Implement basic login correction

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -29,7 +29,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
-    
+
     delete logout_path
     assert_not is_logged_in?
     assert_response :see_other
@@ -39,6 +39,4 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
-end
-
 end
